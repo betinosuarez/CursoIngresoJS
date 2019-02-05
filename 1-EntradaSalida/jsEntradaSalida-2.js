74619cccd9958bb08be366f4bf445d617b432030
alumno <alumno>
2019-02-04 22:16:05 -0300

    Reemplazamos los primeros 3 ejercicios

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,7 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre = prompt("Por favor ingrese un nombre");
+	alert("El nombre ingresado fue " +nombre); //olvidé el "+" para concatenar el valor de "nombre" a la alerta.
 }
 
