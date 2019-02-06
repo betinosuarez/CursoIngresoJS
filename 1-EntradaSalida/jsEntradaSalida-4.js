3e941e1846ad3324f347ddc357dde17692e61579
alumno <alumno>
2019-02-06 19:06:00 -0300

    Ejercicio 4 correjido

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -2,12 +2,11 @@
 	Debemos lograr tomar un dato por 'PROMPT' 
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
+function mostrar()
+{
 	var nombre;
 
 	nombre = prompt("Por favor ingrese un nombre");
-function mostrar()
-{
-
 	elNombre.value = nombre;
 }
 
