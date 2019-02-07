99a9fa4ec190ffd99beeb076ef6b4dd305adddfd
alumno <alumno>
2019-02-06 22:12:59 -0300

    empezado ej 9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,17 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var importe;
+	var aumento;
+	var resultado;
+
+	importe = sueldo.value;
+	importe = parseInt(importe);
+
+	aumento = 1.10
+	resultado = importe * aumento;
+
+	resultado.value = resultado;
 
 	
 }
