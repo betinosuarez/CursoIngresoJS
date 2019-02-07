829cc327160ac84e26b044ccb4d0c7015b036982
alumno <alumno>
2019-02-06 21:25:06 -0300

    queda por resolver ej 7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,65 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
-}
+	var primerNumero;
+ 	var segundoNumero;
+ 	var suma;
 
+ 	primerNumero = numeroUno.value;
+ 	segundoNumero = numeroDos.value;
+
+ 	primerNumero = parseInt(primerNumero);
+ 	segundoNumero = parseInt(segundoNumero);
+
+ 	suma = primerNumero+segundoNumero;
+ 	alert("El resultado de la suma es "+suma);
+}
+/*
 function restar()
 {
-	
-}
+	var primerNumero;
+ 	var segundoNumero;
+ 	var resta;
+
+ 	primerNumero = numeroUno.value;
+ 	segundoNumero = numeroDos.value;
+
+ 	primerNumero = parseInt(primerNumero);
+ 	segundoNumero = parseInt(segundoNumero);
+
+ 	resta = primerNumero - segundoNumero;
+ 	alert("El resultado de la resta es "+resta);
+
 
 function multiplicar()
 { 
-	
+	var primerNumero;
+ 	var segundoNumero;
+ 	var multiplicacion;
+
+ 	primerNumero = numeroUno.value;
+ 	segundoNumero = numeroDos.value;
+
+ 	primerNumero = parseInt(primerNumero);
+ 	segundoNumero = parseInt(segundoNumero);
+
+ 	multiplicacion = primerNumero * segundoNumero;
+ 	alert("El resultado es "+multiplicacion);
 }
 
 function dividir()
 {
-	
+	var primerNumero;
+ 	var segundoNumero;
+ 	var division;
+
+ 	primerNumero = numeroUno.value;
+ 	segundoNumero = numeroDos.value;
+
+ 	primerNumero = parseInt(primerNumero);
+ 	segundoNumero = parseInt(segundoNumero);
+
+ 	division = primerNumero / segundoNumero;
+ 	alert("El resultado de la division es "+division);
 }
 
