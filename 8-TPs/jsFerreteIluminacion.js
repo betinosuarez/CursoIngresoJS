8e5f250a4fc22f19d70e4ef29d3afc6dcdbb706f
betinosuarez <suarez.alberto.n@gmail.com>
2019-02-18 18:05:19 -0300

    Comenzado Iluminacion

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,61 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+ 	var numero;
+ 	var modelo;
+ 	var precio;
+ 	var impuesto;
+
+ 	modelo = Marca.value; 
+ 	precio = 35
+ 	numero = Cantidad.value;
+ 	total = precio * numero;
+
+ 	if(numero > 5){									//6 lamparas
+ 		precio = precio * numero;
+ 		precio = precio + precio * 0.5;
+ 	}
+ 	else{
+ 		if(numero == 5 && modelo == ArgentinaLuz){ //5 lamparas
+ 			precio = precio * numero;
+ 			precio = precio + precio * 0.4;
+ 		}
+ 		else{
+ 			precio = precio * numero;
+ 			precio = precio + precio * 0.3;
+ 		}
+ 		if(numero == 4 && modelo == ArgentinaLuz || numero == 4 && modelo == FelipeLamparas){
+ 			precio = precio * numero;
+ 			precio = precio + precio * 0.25;
+ 		}
+ 		else{
+ 			precio = precio * numero;
+ 			precio = precio + precio * 0.2;
+ 		}
+ 		if(numero == 3 && modelo == ArgentinaLuz){
+ 			precio = precio * numero;
+ 			precio = precio + precio * 0.15;
+ 		}
+ 		else{
+ 			if(numero == 3 && modelo == FelipeLamparas){
+ 				precio = precio * numero;
+ 				precio = precio + precio * 0.1;
+ 			}
+ 			else{
+ 				precio = precio * numero;
+ 				precio = precio + precio * 0.05;
+ 			}
+ 		}
+
+
+ 		if(precio > 120){
+ 			impuesto = precio * 0.1;
+ 			precio = precio + impuesto;
+ 			precioDescuento.value = precio;
+ 			alert("Usted pagó $" + impuesto + "de Ingresos Brutos.");
+ 		}
+ 		else{
+ 			precioDescuento.value = precio;
+ 		}
+ 	}
 }
