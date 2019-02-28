80a8bf531a5be09fa0a520702a286d1d34686634
alumno <alumno>
2019-02-27 22:10:46 -0300

    Realizados for

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,9 +1,79 @@
 function mostrar()
 {
-	var contador;
+	/*var contador;
 	contador = 0;
 	for(;contador < 10;){  //el primero ";" debe estar, de contrario, no toma la formula del for.
 		console.log(contador);
 		contador ++
+	}*/
+
+
+	/*var contador;
+	var numeroRandom;
+	var contadorUno = 0;
+	var contadorDos = 0;
+	var contadorTres = 0;
+	var contadorCuatro = 0;
+	var contadorCinco = 0;
+	var contadorSeis = 0;
+	var contadorSiete = 0;
+	var contadorOcho = 0;
+	var contadorNueve = 0;
+	var contadorDiez = 0;
+
+	for(contador = 0;contador < 10;contador++){
+		numeroRandom = Math.floor((Math.random()*10)+1);
+		numeroRandom = parseInt(numeroRandom);
+
+		console.log(numeroRandom);
+
+		switch(numeroRandom){
+			case 1:
+				contadorUno++
+				break;
+			case 2:
+				contadorDos++
+				break;
+			case 3:
+				contadorTres++
+				break;
+			case 5:
+				contadorCuatro++
+				break;
+			case 6:
+				contadorCinco++
+				break;
+			case 7:
+				contadorSiete++
+				break;
+			case 8:
+				contadorOcho++
+				break;
+			case 9:
+				contadorNueve++
+				break;
+			case 10:
+				contadorDiez++
+				break;
+		}
+	}
+	console.log("El numero 1 salio " + contadorUno + " veces." );
+	console.log("El numero 1 salio " + contadorDos + " veces." );
+	console.log("El numero 1 salio " + contadorTres + " veces." );
+	console.log("El numero 1 salio " + contadorCuatro + " veces." );
+	console.log("El numero 1 salio " + contadorCinco + " veces." );
+	console.log("El numero 1 salio " + contadorSeis + " veces." );
+	console.log("El numero 1 salio " + contadorSiete + " veces." );
+	console.log("El numero 1 salio " + contadorOcho + " veces." );
+	console.log("El numero 1 salio " + contadorNueve + " veces." );
+	console.log("El numero 1 salio " + contadorDiez + " veces." ); */
+
+
+
+	var palabra = "luz azul";
+	
+
+	for(i=0;i<palabra.length;i++){
+		palabra[i];
 	}
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,38 @@
 function mostrar()
 {
+	var nombre;
+	var nombremax;
+	var peso;
+	var pesomax = 0;
+	var temp = 0;
+	var tempmax;
+	var temppar;
+	var subzero;
+	var minzero;
+	var maxzero;
+	var cuenta;
+	var respuesta = "si";
 
+	while(respuesta = "si"){
+		
+			nombre = prompt("Ingrese nombre del animal.");
+			temp = prompt("Ingrese temperatura de su hábitat.")
+			while(temp <-30 || temp > 30){
+				temp = prompt("Temperaturas menores a -30º o mayores a 30º no toleradas.");
+			}
+			peso = prompt("Ingrese el peso del animal.");
+			while(peso > 1000 || peso < 1){
+				peso = prompt("Pesos menores a 1Kg o mayores a 1Tn no tolerados.");
+				if(peso > pesomax){
+					pesomax = peso;
+					nombremax = nombre;
+					tempmax = temp;
+				}
+			}
+			respuesta=prompt("Ingrese "+ "si " + "para cargar datos de otro animal.");
 }
+/*a) La cantidad de temperaturas pares. 
+b) El nombre y temperatura del animal más pesado 
+c) La cantidad de animales que viven a menos de 0 grados. 
+d) El promedio del peso de todos los animales.	
+f) El peso máximo y el mínimo de todos los animales cuyas temperaturas sean bajo cero.*/
