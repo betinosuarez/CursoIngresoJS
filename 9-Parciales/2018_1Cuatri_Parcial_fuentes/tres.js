2a002cf49619afcb31db94ab114d2f98049a2815
alumno <alumno>
2019-03-02 10:17:27 -0300

    repaso ej tres completado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,21 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var descuento;
+	var total;
 
+	precio = prompt("Ingrese el precio del producto.");
+	porcentaje = prompt("Ingrese el porcentaje de descuento.");
+
+	precio = parseFloat(precio);
+	porcentaje = parseInt(porcentaje);
+
+	descuento = precio * porcentaje/100;
+
+	total = precio - descuento;
+
+	elPrecioFinal.value = total;
 }
+
+//Pedir por prompt el precio y el porcentaje de descuento, mostrar el precio final con descuento por id.
